e96e1a793f33d42d94c32378f2da012d64e14b7e
piyamon <girlpiyamon@gmail.com>
2016-06-15 17:02:17 -0700

    ทำมั้ยดั้ย

--- app/src/main/java/k/piyamon/pbrurun/MapsActivity.java
+++ app/src/main/java/k/piyamon/pbrurun/MapsActivity.java
@@ -5,6 +5,8 @@ import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,8 +15,21 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -22,11 +37,21 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private double userLatADouble, userLngADouble;
     private LocationManager locationManager;
     private Criteria criteria;
+    private String[] userLoginStrings;
+    private MyData myData;
+    private static final String urlEditLocation = "http://swiftcodingthai.com/pbru3/edit_location.php";
+    private static final String urlUser = "http://swiftcodingthai.com/pbru3/get_user.php";
+    private int[] avataInts,buildInts;
+    private double[] buildLatDoubles, buildLngDoubles;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        userLoginStrings = getIntent().getStringArrayExtra("Login");
+
+        myData = new MyData();
 
         //Setup Location
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -40,6 +65,59 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     } //Main Method
+
+    private class ConnectedLocation extends AsyncTask<Void, Void, String> {
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlUser).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            } catch (Exception e){
+                return null;
+            }
+
+        } //doIn
+
+        @Override
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+
+            Log.d("pbruV6", "s ==> " + s);
+
+            try {
+
+                JSONArray jsonArray = new JSONArray(s);
+                for (int i=0;i<jsonArray.length();i++){
+
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    double douLat = Double.parseDouble(jsonObject.getString("Lat"));
+                    double douLng = Double.parseDouble(jsonObject.getString("Lng"));
+                    LatLng latLng = new LatLng(douLat, douLng);
+                    String strTitle = jsonObject.getString("Name");
+                    int intIndex = Integer.parseInt(jsonObject.getString("Avata"));
+
+                    mMap.addMarker(new MarkerOptions()
+                    .position(latLng)
+                    .title(strTitle)
+                    .icon(BitmapDescriptorFactory.fromResource(avataInts[intIndex])));
+
+                } // for
+
+
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+
+        } //pnPost
+    }
+
 
     @Override
     protected void onResume() {
@@ -86,7 +164,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
 
-
         return location;
     } //myFindLocaton
 
@@ -115,7 +192,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     };
 
-
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -125,13 +201,76 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //Setup Center map
+        LatLng latLng = new LatLng(myData.getLatADouble(), myData.getLngADouble());
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+
+        //Create My Loop
+        createMyLoop();
+
+    } //onMapReady
+
+    private void createMyLoop(){
+        editUserLocationToServer();
+
+        makAllMarker();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                createMyLoop();
+            }
+        }, 3000);
     }
-}//
+
+    private void makAllMarker() {
+
+        mMap.clear();
+
+        ConnectedLocation connectedLocation = new ConnectedLocation();
+        connectedLocation.execute();
+
+        buildInts = myData.getBuildIconInts();
+        buildLatDoubles = myData.getBuildLatDoubles();
+        buildLatDoubles = myData.getBuildLngDonles();
+
+        for (int i=0;i<buildInts.length;i++){
+            mMap.addMarker(new MarkerOptions())
+                    .position(new LatLng(buildLatDoubles[i], buildLngDoubles[i]))
+                    .icon(BitmapDescriptorFactory.fromResource(buildInts[i]));
+        }
+
+    } // mak
+
+    private void editUserLocationToServer() {
+
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("id", userLoginStrings[0])
+                .add("Lat", Double.toString(userLatADouble))
+                .add("Lng", Double.toString(userLngADouble))
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlEditLocation).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+
+            }
+        });
+    }
+} // Main Class
--- app/src/main/java/k/piyamon/pbrurun/MyData.java
+++ app/src/main/java/k/piyamon/pbrurun/MyData.java
@@ -8,9 +8,22 @@ public class MyData {
     //Explicit
     private int[] avataInts = new int[] {R.drawable.bird48,
     R.drawable.doremon48,R.drawable.kon48,R.drawable.nobita48,R.drawable.rat48};
+    private double latADouble = 13.069937,lngADouble = 99.978201;
+    private double[] buildLatDoubles = new double[]{13.069937,99.978201};
+    private double[] buildKngDoules = new double[]{99.978201,13.069937};
+    private int[] buildIconInts = new int[]{R.drawable.th, R.drawable.sg};
 
-     public int[] getAvataInts(){
-         return avataInts;
-     }
+    public int[] getAvataInts(){
+        return avataInts;
+    }
+
+    public double getLatADouble() {
+        return latADouble;
+    }
+
+    public double getLngADouble() {
+        return lngADouble;
+    }
+
 
 } // Main Class
